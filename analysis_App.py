f6e6cd4d72ea878e080246ee1c14840e6ad43d1b
dvc33 <108354943+dvc33@users.noreply.github.com>
2025-07-23 13:00:02 +0530

    Update analysis_App.py

--- analysis_App.py
+++ analysis_App.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 st.header("Sentiment_Analysis")
 
 st.title("Using NLP via predicting analysis")
@@ -52,8 +55,6 @@ with st.container():
 
 
 
-import matplotlib.pyplot as plt
-import numpy as np
 rand = np.random.normal(1, 2, size=20)
 fig, ax = plt.subplots()
 ax.hist(rand, bins=15)
